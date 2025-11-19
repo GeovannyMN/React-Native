18a10feb6f371dcb1fbb088cbbc2242317518336
GeovannyMN <48793542+GeovannyMN@users.noreply.github.com>
2025-11-19 10:51:45 -0300

    Create ArrayViaFlatList.tsx

---
+++ ArrayViaFlatList.tsx
@@ -0,0 +1,103 @@
+import { StyleSheet, Text, View, Image, FlatList} from 'react-native';
+
+const ArrayUser = [
+  {
+    nome: 'Jardani',
+    profissao: 'Analista'
+  },
+  {
+    nome: 'Gardenne',
+    profissao: 'Stakeholder'
+  },
+  {
+    nome: 'Janaina',
+    profissao: 'Gerente de Projeto'
+  },
+  {
+    nome: 'Garfi',
+    profissao: 'Desenvolvedor'
+  },
+  {
+    nome: 'Jorge',
+    profissao: 'Stakeholder'
+  }
+]
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+      {//_renderCard()
+      }
+
+      <FlatList
+        data = {ArrayUser}
+        keyExtractor = {(item, index) => index.toString()}
+        renderItem={({item, index})=>{
+          return (
+          <View style={styles.card} key={index}>
+            <View style={styles.boxImg}>
+              <Image
+                resizeMode= 'cover'
+                style={{width:'100%', height:'100%'}}
+                source={{uri:`https://robohash.org/${index}.png`}}
+              />
+            </View>
+          <View style={styles.box}>
+            <Text style={styles.titulo}>  Nome:</Text>
+            <Text style={styles.descricao}>    {item.nome}</Text>
+            <Text style={styles.titulo}>  Profissão:</Text>
+            <Text style={styles.descricao}>    {item.profissao}</Text>
+          </View>
+        </View>
+        )
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#abf',
+    alignItems: 'center',
+    paddingVertical: 60,
+    paddingHorizontal: 40
+  },
+  card: {
+    width: '100%',
+    height: 200,
+    backgroundColor: '#aff',
+    borderRadius: 15,
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 10
+  },
+  box: {
+    borderWidth:1,
+    width: '70%',
+    height: '95%',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.4,
+    shadowRadius: 8.3
+  },
+  boxImg: {
+    borderWidth:1,
+    width: '30%',
+    height: '95%'
+  },
+  imgs: {
+    
+  },
+  titulo: {
+    color: '#00f'
+  },
+  descricao: {
+    fontWeight: 'bold'
+  }
+});
